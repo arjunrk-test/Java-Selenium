c8b900e3e270223775a7e10dd3fc1e135f588537
arjunrk-test <arjunrk.testing@gmail.com>
2025-01-18 08:21:13 +0530

    Added a basic test to check the dataproviders

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,4 +1,31 @@
 package Pages;
+import BaseClass.DataProvider;
+import PagesTest.LoginPageTest;
+import Utility.LoginUtil;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.PrintStream;
 
 public class LoginPage {
+    @Test(enabled = true, dataProvider = "chrome", dataProviderClass = DataProvider.class)
+    public void checkLogin(String browser) throws InterruptedException, IOException{
+        try{
+            PrintStream console = System.out;
+            LoginUtil.login(browser);
+            LoginPageTest.loginTest();
+            System.out.println(console);
+        }
+        catch(Exception | AssertionError e){
+            Assert.assertEquals(1, 2);
+        }
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        System.out.println("Closing the browser");
+        LoginUtil.close();
+    }
 }

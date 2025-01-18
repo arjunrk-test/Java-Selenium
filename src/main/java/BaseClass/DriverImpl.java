f97e770554262202e159d5f83e8e97903c3c1f80
arjunrk-test <arjunrk.testing@gmail.com>
2025-01-18 08:50:02 +0530

    Updated the code to choose the drivers according to the os

--- src/main/java/BaseClass/DriverImpl.java
+++ src/main/java/BaseClass/DriverImpl.java
@@ -1,5 +1,4 @@
 package BaseClass;
-
 import Utility.GlobalVariables;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,20 +6,29 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverImpl {
     public static WebDriver driver;
+    private String driverPath;
 
     @SuppressWarnings("deprecation")
-    public DriverImpl(int flag){
-        if(flag == 1){
-            System.setProperty("webdriver.chrome.driver", GlobalVariables.chromeDriverpath);
+    public DriverImpl(int flag) {
+        String osInfo = System.getProperty("os.name").toLowerCase();
+        driverPath = getDriverPath(osInfo, flag);
+        if (flag == 1) {
+            System.setProperty("webdriver.chrome.driver", driverPath);
             driver = new ChromeDriver();
-            driver.manage().deleteAllCookies();
-            driver.manage().window().maximize();
-        }
-        else{
-            System.setProperty("webdriver.edge.driver", GlobalVariables.edgeDriverPath);
+        } else {
+            System.setProperty("webdriver.edge.driver", driverPath);
             driver = new EdgeDriver();
-            driver.manage().deleteAllCookies();
-            driver.manage().window().maximize();
+        }
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+    }
+
+    private String getDriverPath(String osInfo, int flag) {
+        if (flag == 1) {
+            return osInfo.contains("windows") ? GlobalVariables.chromeDriverPathWin : GlobalVariables.chromeDriverPathMac;
+        } else {
+            return osInfo.contains("windows") ? GlobalVariables.edgeDriverPathWin : GlobalVariables.edgeDriverPathMac;
         }
     }
 }
+
--- src/main/java/Utility/GlobalVariables.java
+++ src/main/java/Utility/GlobalVariables.java
@@ -3,8 +3,10 @@ package Utility;
 public class GlobalVariables {
     public static String userName = System.getProperty("user.name");
     public static String projectDir = System.getProperty("user.dir");
-    public static String chromeDriverpath = projectDir + "\\Drivers\\chromedriver.exe";
-    public static String edgeDriverPath = projectDir + "\\Drivers\\msedgedriver.exe";
+    public static String chromeDriverPathWin = projectDir + "\\Drivers\\chromedriver.exe";
+    public static String edgeDriverPathWin = projectDir + "\\Drivers\\msedgedriver.exe";
+    public static String chromeDriverPathMac = projectDir + "\\Drivers\\chromedriver";
+    public static String edgeDriverPathMac = projectDir + "\\Drivers\\msedgedriver";
     public static String url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
     public static String loginUserName = "Admin";
     public static String loginUserPassword = "admin123";
